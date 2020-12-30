4845dfda3da576fc2f87b900cc505ebe815ee54e
yktk435 <yokotsuka435@gmail.com>
2020-12-30 22:52:22 +0900

    2020/12/30

--- C問題集/code.c
+++ C問題集/code.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
+#define NU 7
+
+// 学生の点数のデータ
+int point[NU];
+// 合格者を表示
+void print_passed_student(void);
+
 int main(void)
 {
-    int no;
-    while (1)
+    int i;
+    extern int point[NU];
+    puts("7人の点数を入力");
+    for (i = 0; i < NU; i++)
     {
-        scanf("%d", &no);
-        if (no > 0)
-            printf("+");
-        else
-            printf("-");
-
-        printf("\n");
+        printf("%d: ", i + 1);
+        scanf("%d", &point[i]);
     }
+    print_passed_student();
+    return 0;
+}
+
+void print_passed_student(void)
+{
+    int i;
+    extern int point[NU];
+    puts("合格者一覧表");
+    puts("----------");
+    for (i = 0; i < NU; i++)
+    {
+        if (point[i] >= 60)
+            printf("%d番目 %d\n", i + 1, point[i]);
+    }
+    puts("----------");
 }
